f57ae2764901cc4906da66d4891c41ac7a34f3a1
tributetothemoon <kkm97351@gmail.com>
2020-09-10 03:14:44 +0900

    feat : 책 검색 함수 작성 네이버 OPEN API를 이용하여 책을 검색하는 views 작성.

--- booknet/views.py
+++ booknet/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import json, requests
 
 def index(request):
     return render(request, 'index.html')
+
+def searchBook(request, query):
+    url = "https://openapi.naver.com/v1/search/book.json?"
+
+    # 네이버 OpenAPI 신청 후 키 발급 가능 https://developers.naver.com/apps/#/register
+    id = "J4k1lEwGVJ575y1ICGF6"
+    secret = "zy67Ui2yNc"
+
+    header = {"X-Naver-Client-Id": id, "X-Naver-Client-Secret": secret}
+
+    queryStr = "query=" + query  # 검색어
+
+    response = requests.get(url + query, headers=header).json()
+
+    print(response)
