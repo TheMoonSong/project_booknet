5d42c39736ba619e81fb82d9115c7cd49b2e6c1c
tributetothemoon <kkm97351@gmail.com>
2020-09-12 22:44:45 +0900

    refactor : app별로 static file을 관리하도록 디렉터리 구조 변경

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## version info
 - Python 3
 - Django 2.1.5
+- requests 2.24
--- booknet/settings.py
+++ booknet/settings.py
@@ -118,8 +118,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'booknet', 'static'),
+    os.path.join(BASE_DIR,'accounts', 'static'),
     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
