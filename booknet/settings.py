54e4f4a7aa9da3d490f0acd8999c47788c97b5b3
tributetothemoon <kkm97351@gmail.com>
2020-09-14 22:52:01 +0900

    feat : add 'feed' app

--- booknet/settings.py
+++ booknet/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'booknet', 'static'),
     os.path.join(BASE_DIR,'accounts', 'static'),
+    os.path.join(BASE_DIR,'feed', 'static'),
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
