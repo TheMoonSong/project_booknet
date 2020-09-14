ee81ebce5dce9656e60f73b3fc386822941b1ee7
tributetothemoon <kkm97351@gmail.com>
2020-09-15 04:14:00 +0900

    feat : 피드 업로드 및 쿼리-리스팅 API 구현

--- booknet/urls.py
+++ booknet/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from booknet import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('accounts/', include("accounts.urls")),
     path('search/<str:query>', views.searchBook, name='searchBook'),
+    path('feed/', include("feed.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
+import os
+
+def image_upload_to(instance, filename):
+    base_path = os.path.abspath('media')
+    path = os.path.join(base_path, str(instance.isbn))
+    name, ext = filename.split('.')
+    print(os.path.join(path, '%s.%s'%(name, ext)))
+    return os.path.join(path, '%s.%s'%(name, ext))
+
+class Feed(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    text = models.TextField(blank=True)
+    isbn = models.IntegerField()
+    image = models.ImageField(upload_to='timeline')
+    created = models.DateTimeField(auto_now_add=True)
 
 # Create your models here.
