6bf76f96389ea18929f5c47ad9385830fcce1748
Kimun Kim <kkm97351@gmail.com>
2021-07-16 22:41:20 +0900

    refactor: 레거시 프로젝트 삭제

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    avatar = models.ImageField(null=True)
-
-# Create your models here.
