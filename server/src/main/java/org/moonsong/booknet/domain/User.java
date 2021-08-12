c19f2d38517fd718bb51f174d6994ab61cf4c773
Kimun Kim <kkm97351@gmail.com>
2021-08-12 17:51:42 +0900

    feat: User 엔티티 생성

---
+++ server/src/main/java/org/moonsong/booknet/domain/User.java
@@ -0,0 +1,31 @@
+package org.moonsong.booknet.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    public User(Long id, String email, String password, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
