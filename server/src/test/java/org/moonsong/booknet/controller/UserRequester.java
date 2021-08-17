21ddb0d662517a9fa34b880e44b2f31772407244
Kimun Kim <kkm97351@gmail.com>
2021-08-17 19:56:52 +0900

    feat: Rest Docs 관련 인수테스트 설정

--- server/src/test/java/org/moonsong/booknet/controller/UserRequester.java
+++ server/src/test/java/org/moonsong/booknet/controller/UserRequester.java
@@ -6,11 +6,15 @@ import io.restassured.response.Response;
 import org.moonsong.booknet.dto.UserCreateRequest;
 import org.springframework.http.MediaType;
 
+import static org.moonsong.booknet.DocumentUtils.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
 public class UserRequester {
     static ExtractableResponse<Response> createUserRequest(UserCreateRequest userCreateRequest) {
         return RestAssured
-                .given().log().all()
+                .given(getRequestSpecification()).log().all()
                 .accept("application/json")
+                .filter(document("member/create", getRequestPreprocessor(), getResponsePreprocessor()))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(userCreateRequest)
                 .when().post("/api/users")
